7f8591241ce62c6d89ae9d9e400a4bb926702b8b
CKPRASHANTH07 <ckprashanthc@gmail.com>
2024-02-04 12:43:05 +0530

    nav

---
+++ src/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='flex '>make-my-trip
+
+    <button>Busses</button>
+    <button>Flights</button>
+    <button>Hotels</button>
+    <button>Homestays</button>
+    <button>Holiday packages</button>
+    <button>Trains</button>
+    <button>Busses</button>
+    <button>Cabs</button>
+
+    <button>Forex-card & Currency</button>
+    <button>MYBIZ Buisness Travel Solution</button>
+
+    <button>travel insurance</button>
+    <button>Manage Bookings </button>
+
+    <button>Login/Create </button>
+
+    </div>
+    </>
+    )
+}
+
+export default Navbar
