63b7afb1914c324221ce03be3e918014bbdbaf2c
CKPRASHANTH07 <ckprashanthc@gmail.com>
2024-02-12 14:40:25 +0530

    modified:   src/Page4.jsx

--- src/Page4.jsx
+++ src/Page4.jsx
@@ -4,43 +4,43 @@ const Page4 = () => {
   return (
     <div className=" flex flex-col bg-slate-200 h-screen  font-sans">
       <h1 className='p-20'>FEATURED<br/>ENGAGEMENTS</h1>
-      <div className='flex p-20 h-60 overflow-x-auto overflow-hidden'>
+      <div className='flex overflow-x-auto overscroll-auto overflow-hidden mb-10'>
     
 
 
     <div className=' flex'>
       
-    <div className='  mt-20 ml-6 h-[450px] w-[300px]'>
+    <div className='  mt-10 ml-6 h-[450px] w-[300px]'>
          <h1 className=' underline pl-10'>GOOGLE</h1>
         
          <h3 className='pt-10 pl-5 pr-5 text-gray-600 font-sans text-[15px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h3>
 
     </div>
-    <div className='  mt-20 ml-6 h-[450px] w-[300px]  rounded-2xl'>
+    <div className='  mt-10 ml-6 h-[450px] w-[300px]  rounded-2xl'>
     <h1 className='pl-10 underline '>GOOGLE</h1>
   
     <h3 className='pt-10 pl-5 pr-5 text-gray-600 font-sans text-[15px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h3>
 
     </div>
-    <div className='  mt-20 ml-6 h-[450px] w-[300px]  rounded-2xl '>
+    <div className='  mt-10 ml-6 h-[450px] w-[300px]  rounded-2xl '>
     <h1 className='pl-10 underline '>GOOGLE</h1>
   
     <h3 className='pt-10 pl-5 pr-5 text-gray-600 font-sans text-[15px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h3>
 
     </div>
-    <div className='  mt-20 ml-6 h-[450px] w-[300px]  rounded-2xl '>
+    <div className='  mt-10 ml-6 h-[450px] w-[300px]  rounded-2xl '>
     <h1 className='pl-10 underline '>GOOGLE</h1>
   
     <h3 className='pt-10 pl-5 pr-5 text-gray-600 font-sans text-[15px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h3>
 
     </div>
-    <div className='  mt-20 ml-6 h-[450px] w-[300px]  rounded-2xl '>
+    <div className='  mt-10 ml-6 h-[450px] w-[300px]  rounded-2xl '>
     <h1 className='pl-10 underline '>GOOGLE</h1>
   
     <h3 className='pt-10 pl-5 pr-5 text-gray-600 font-sans text-[15px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h3>
 
     </div>
-    <div className='  mt-20 ml-6 h-[450px] w-[300px]  rounded-2xl '>
+    <div className='  mt-10 ml-6 h-[450px] w-[300px]  rounded-2xl '>
     <h1 className='pl-10 underline '>GOOGLE</h1>
   
     <h3 className='pt-10 pl-5 pr-5 text-gray-600 font-sans text-[15px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h3>
